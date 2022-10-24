3253983f7e13ace4df774464c67b60ea0ad030af
ferioo <116171245+ferioo@users.noreply.github.com>
2022-10-24 19:48:45 +0700

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,10 @@
+public class Main{
+    public static void main(String[] args) {
+        for(int i = 1; i <= 10; i++)
+        if (i % 2==0){
+            System.out.println("angka " + i + " merupakan angka genap");
+        }
+        else
+            System.out.println("angka " + i + " merupakan angka ganjil");
+    }
+}
